eca46497eddbf15d59863d693edca6eaf2426a8a
James Murphy <jmurphy4@mail.umw.edu>
2014-12-11 16:50:46 -0500

    Extracts strings from file.

--- quickgen.py
+++ quickgen.py
@@ -6,41 +6,19 @@ fout = open('output.txt', 'w')
 
 random.seed(None)
 
-shortv = ["a", "e", "i", "o", "u", "y"]
-longv = ["á", "é", "í", "ó", "ú", "ý"]
-vowels = shortv + longv
+phonemes = []
 
-plosivesV = ["b", "d", "g"]
-plosivesVl = ["p", "t", "k"]
-plosives = plosivesVl + plosivesV
+with open('input.txt') as fin:
 
-fricativesV = ["v", "z", "ð"]
-fricativesVl = ["f", "s", "þ"]
-fricatives = fricativesV + fricativesVl
+	for line in fin:
+		list = line.split()
+		if list[0][0] == '#':
+			break
+		elif list[0][0] != '/':
+			phonemes.append(list)
 
-obstruents = plosives + fricatives
+print "%s \n" % phonemes[0]
+print "%s \n" % phonemes[1]
+print "%s \n" % phonemes[2]
 
-nasals = ["m", "n", "ñ"]
-liquids = ["l", "r"]
-glides = ["j", "w"]
-approximants = liquids + glides
-sonorants = liquids + nasals + glides
-
-consonants = obstruents + sonorants
-
-i = raw_input("Number of words you'd like to make? ")
-print '\n'
-i = int(float(i))
-
-while i >= 0:
-	p = int(float(random.random() * 5)) + 2
-	word = ""
-	while p >= 0:
-		if p % 2 == 1:
-			word += consonants[int(float(random.random() * 100)) % len(consonants)]
-		elif p % 2 == 0:
-			word += vowels[int(float(random.random() * 100)) % len(vowels)]
-		p = p - 1
-	fout.write(word)
-	fout.write('\n')
-	i = i - 1
+print phonemes[2][4]
