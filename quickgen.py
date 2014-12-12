178ba733dd92a640e5e95d5810fc88a547d5d2de
James Murphy <jmurphy4@mail.umw.edu>
2014-12-11 19:58:15 -0500

    Generates Hawaiian-like words. Need to separate the extraction of consonants and vowels.

--- quickgen.py
+++ quickgen.py
@@ -8,7 +8,7 @@ random.seed(None)
 
 phonemes = []
 rules = []
-syllables = ['ONC', 'ON', 'NC', 'N']
+syllables = ['ON', 'N']
 
 with open('input.txt') as fin:
 
@@ -26,3 +26,26 @@ with open('input.txt') as fin:
 		elif list[0][0] != '/':
 			rules.append(list)
 
+i = int(float(raw_input("How many words would you like to build? ")))
+
+while i > 0:
+	word = ''
+	index = int(random.random() * 100) % len(syllables)
+	form = syllables[index]
+	numsyll = int(random.random() * 100) % 4 + 1
+	for k in range(0, numsyll):
+		syll = ''
+		for j in range(0,  len(form)):
+			decide = 'O'
+			if form[j] == 'O':
+				decide = int(random.random() * 100) % 2
+			elif form[j] == 'N':
+				decide = 2
+			elif form[j] == 'C':
+				decide = 1
+			chardex = int(random.random() * 100) % len(phonemes[decide])
+			syll += phonemes[decide][chardex]
+		word += syll
+	fout.write(word)
+	fout.write('\n')
+	i = i - 1
