d6d7cc367a84d96db18ed613c3167f185b913cb1
James Murphy <jmurphy4@mail.umw.edu>
2015-01-16 16:00:02 -0500

    Added start to cleanup.py and rules.txt

---
+++ cleanup.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python -tt
+# -*- coding: utf-8 -*-
+import os, sys
+
+#supply input as raw_input if running Python 3 or higher
+if sys.version_info >= (3,0):
+	raw_input = input
+
+#ask for name of input file of cleanup rules
+rules = raw_input("What is the name of your cleanup rules file? (Leave blank for 'rules.txt') ")
+if rules == "":
+	rules = "rules.txt"
+
+#ask for name of input file of raw wordgen output
+words = raw_input("What is the name of your file of raw wordgen output? (Leave blank for 'output.txt') ")
+if words == "":
+	words = "output.txt"
+
+#ask for the name of output file for clean wordgen
+clean = raw_input("What is the name of your cleaned output file? (Leave blank for 'clean.txt') ")
+if clean = "":
+	clean = "clean.txt"
+
