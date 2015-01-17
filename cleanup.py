1c05e9fc67a2e8c4e361744ce3a73e66e0463ae4
James Murphy <jmurphy4@mail.umw.edu>
2015-01-16 22:09:15 -0500

    cleanup.py improved, but still in progress.

--- cleanup.py
+++ cleanup.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python -tt
 # -*- coding: utf-8 -*-
-import os, sys
+import os, sys, string
 
 #supply input as raw_input if running Python 3 or higher
 if sys.version_info >= (3,0):
@@ -18,6 +18,60 @@ if words == "":
 
 #ask for the name of output file for clean wordgen
 clean = raw_input("What is the name of your cleaned output file? (Leave blank for 'clean.txt') ")
-if clean = "":
+if clean == "":
 	clean = "clean.txt"
 
+#prepare list of rewrite rules
+rewrites = []
+
+#prepare output file
+fout = open(clean, 'w')
+
+#extract from rule file
+with open(rules) as fin:
+	#get rules line-by-line
+	for line in fin:
+		#skip blank lines
+		if line.strip() == "":
+			continue
+		#split rewrite conditions from rules
+		list = line.split()
+		
+		#if a line has an isolated # sign in it, stop extraction
+		if list[0][0] == "#":
+			break
+
+		#and if there's no slash, append it to the list
+		elif list[0][0] != '/':
+			rewrites.append(list)
+fin.close()
+
+
+#pull one string from the file at a time
+with open(words) as fin:
+	#start loop to rewrite each word in file
+	for line in fin:
+		#prepare word variable
+		word = line
+		cleanword = ""
+		#step through all rewrite rules
+		for i in range (0, len(rewrites)):
+			for j in range (0, len(rewrites[i]) - 1):
+				#variable to hold current substring
+				substring = rewrites[i][j]
+				#variable to index to replacement substring
+				replace = len(rewrites[i]) - 1
+				#check if substring is in the word
+				if substring in word:
+					#replace it
+					cleanword = string.replace(word, substring, rewrites[i][replace])
+		#write word to file
+		fout.write(cleanword)
+
+#close files
+fin.close()
+fout.close()
+
+sys.stdout.write("Program finished. \n")
+
+#end program
